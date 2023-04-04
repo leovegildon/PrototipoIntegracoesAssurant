7781c62ad8c58184ed48d76588f9f3eae7c7ada2
Leovegildo Ferreira de Araujo Neto <leovegildon@gmail.com>
2023-04-04 11:53:39 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace PrototipoIntegracoesAssurant
+{
+    partial class FormPai
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Monitoração de Envios para Seguradora");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Integração Assurant", new System.Windows.Forms.TreeNode[] {
+            treeNode1});
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("EDI", new System.Windows.Forms.TreeNode[] {
+            treeNode2});
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.arquivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eDIToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eDIToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.integraçãoAXAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabelaDeEnquadramentoDeProdutosGarantiaEstendidaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.monitoraçãoDeEnvioParaSeguradoraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.controleDoSistemaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.arquivoToolStripMenuItem,
+            this.eDIToolStripMenuItem,
+            this.eDIToolStripMenuItem1,
+            this.controleDoSistemaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1344, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // arquivoToolStripMenuItem
+            // 
+            this.arquivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sairToolStripMenuItem});
+            this.arquivoToolStripMenuItem.Name = "arquivoToolStripMenuItem";
+            this.arquivoToolStripMenuItem.Size = new System.Drawing.Size(121, 20);
+            this.arquivoToolStripMenuItem.Text = "Gerência de Vendas";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            // 
+            // eDIToolStripMenuItem
+            // 
+            this.eDIToolStripMenuItem.Name = "eDIToolStripMenuItem";
+            this.eDIToolStripMenuItem.Size = new System.Drawing.Size(110, 20);
+            this.eDIToolStripMenuItem.Text = "Exportação Fiscal";
+            // 
+            // eDIToolStripMenuItem1
+            // 
+            this.eDIToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.integraçãoAXAToolStripMenuItem});
+            this.eDIToolStripMenuItem1.Name = "eDIToolStripMenuItem1";
+            this.eDIToolStripMenuItem1.Size = new System.Drawing.Size(36, 20);
+            this.eDIToolStripMenuItem1.Text = "EDI";
+            // 
+            // integraçãoAXAToolStripMenuItem
+            // 
+            this.integraçãoAXAToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tabelaDeEnquadramentoDeProdutosGarantiaEstendidaToolStripMenuItem,
+            this.monitoraçãoDeEnvioParaSeguradoraToolStripMenuItem});
+            this.integraçãoAXAToolStripMenuItem.Name = "integraçãoAXAToolStripMenuItem";
+            this.integraçãoAXAToolStripMenuItem.Size = new System.Drawing.Size(179, 22);
+            this.integraçãoAXAToolStripMenuItem.Text = "Integração Assurant";
+            this.integraçãoAXAToolStripMenuItem.Click += new System.EventHandler(this.integraçãoAXAToolStripMenuItem_Click);
+            // 
+            // tabelaDeEnquadramentoDeProdutosGarantiaEstendidaToolStripMenuItem
+            // 
+            this.tabelaDeEnquadramentoDeProdutosGarantiaEstendidaToolStripMenuItem.Name = "tabelaDeEnquadramentoDeProdutosGarantiaEstendidaToolStripMenuItem";
+            this.tabelaDeEnquadramentoDeProdutosGarantiaEstendidaToolStripMenuItem.Size = new System.Drawing.Size(387, 22);
+            this.tabelaDeEnquadramentoDeProdutosGarantiaEstendidaToolStripMenuItem.Text = "Tabela de Enquadramento de Produtos (Garantia Estendida)";
+            // 
+            // monitoraçãoDeEnvioParaSeguradoraToolStripMenuItem
+            // 
+            this.monitoraçãoDeEnvioParaSeguradoraToolStripMenuItem.Name = "monitoraçãoDeEnvioParaSeguradoraToolStripMenuItem";
+            this.monitoraçãoDeEnvioParaSeguradoraToolStripMenuItem.Size = new System.Drawing.Size(387, 22);
+            this.monitoraçãoDeEnvioParaSeguradoraToolStripMenuItem.Text = "Monitoração de Envio para Seguradora";
+            // 
+            // controleDoSistemaToolStripMenuItem
+            // 
+            this.controleDoSistemaToolStripMenuItem.Name = "controleDoSistemaToolStripMenuItem";
+            this.controleDoSistemaToolStripMenuItem.Size = new System.Drawing.Size(126, 20);
+            this.controleDoSistemaToolStripMenuItem.Text = "Controle do Sistema";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::PrototipoIntegracoesAssurant.Properties.Resources.main3;
+            this.pictureBox4.Location = new System.Drawing.Point(0, 162);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(646, 453);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::PrototipoIntegracoesAssurant.Properties.Resources.footer;
+            this.pictureBox3.Location = new System.Drawing.Point(0, 614);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(1343, 46);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::PrototipoIntegracoesAssurant.Properties.Resources.main2;
+            this.pictureBox2.Location = new System.Drawing.Point(645, 162);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(698, 498);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PrototipoIntegracoesAssurant.Properties.Resources.main1;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1343, 139);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // treeView1
+            // 
+            this.treeView1.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.treeView1.Location = new System.Drawing.Point(0, 162);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Nó2";
+            treeNode1.Text = "Monitoração de Envios para Seguradora";
+            treeNode2.Name = "Nó1";
+            treeNode2.Text = "Integração Assurant";
+            treeNode3.BackColor = System.Drawing.Color.Transparent;
+            treeNode3.Name = "Nó0";
+            treeNode3.Text = "EDI";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode3});
+            this.treeView1.Size = new System.Drawing.Size(646, 453);
+            this.treeView1.TabIndex = 6;
+            // 
+            // FormPai
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(1344, 661);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "FormPai";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormPai";
+            this.Load += new System.EventHandler(this.FormPai_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem arquivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eDIToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.ToolStripMenuItem eDIToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem integraçãoAXAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem controleDoSistemaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tabelaDeEnquadramentoDeProdutosGarantiaEstendidaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem monitoraçãoDeEnvioParaSeguradoraToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrototipoIntegracoesAssurant
+{
+    public partial class FormPai : Form
+    {
+        public FormPai()
+        {
+            InitializeComponent();
+        }
+
+        private void FormPai_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void abre_form_filho(Form formulario)
+        {
+
+            formulario.MdiParent = this;
+            formulario.StartPosition = FormStartPosition.CenterParent;
+            formulario.WindowState = FormWindowState.Maximized;
+            formulario.Show();
+        }
+
+        private void integraçãoAXAToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
